a945bbbb4dad57e8f38621069661340fb14d9764
liushuijing <liushuijinger@163.com>
2021-12-14 00:27:24 +0800

    完善es代码

---
+++ 13-es/src/main/java/com/shuijing/boot/es/config/RestClientConfig.java
@@ -0,0 +1,22 @@
+package com.shuijing.boot.es.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+//@Configuration
+public class RestClientConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+            .connectedTo("localhost:9200")
+            .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
--- 13-es/src/main/java/com/shuijing/boot/es/controller/ElasticsearchController.java
+++ 13-es/src/main/java/com/shuijing/boot/es/controller/ElasticsearchController.java
@@ -2,12 +2,14 @@ package com.shuijing.boot.es.controller;
 
 import com.shuijing.boot.es.common.Result;
 import com.shuijing.boot.es.entity.es.Article;
+import com.shuijing.boot.es.repository.ArticleRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
@@ -45,15 +47,24 @@ public class ElasticsearchController {
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
 
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @PostMapping
     @ApiOperation("新增")
     public Result<Iterable<Article>> create(@RequestBody List<Article> articleList) {
-        return Result.success(elasticsearchRestTemplate.save(articleList));
+//        return Result.success(elasticsearchRestTemplate.save(articleList));
+        return Result.success(articleRepository.saveAll(articleList));
     }
 
-    @PutMapping
-    @ApiOperation("更新")
+    @PutMapping("/update-repo")
+    @ApiOperation("通过Repository更新")
+    public Result<Article> update(@RequestBody Article article) {
+        return Result.success(articleRepository.save(article));
+    }
+
+    @PutMapping("/update-temp")
+    @ApiOperation("通过Template更新")
     public Result<UpdateResponse> update(@RequestParam String id,
                                          @RequestParam String title,
                                          @RequestParam String content) {
@@ -68,21 +79,39 @@ public class ElasticsearchController {
                 .withDocument(document)
                 .build();
 
+        articleRepository.save(new Article());
         return Result.success(elasticsearchRestTemplate.update(updateQuery,IndexCoordinates.of("article")));
     }
 
 
-    @DeleteMapping
-    @ApiOperation("删除")
+    @DeleteMapping("del-repo")
+    @ApiOperation("通过Repository删除")
+    public Result<Boolean> delete(@RequestParam Long id) {
+        articleRepository.deleteById(id);
+        return Result.success(Boolean.TRUE);
+    }
+
+    @DeleteMapping("del-temp")
+    @ApiOperation("通过Template删除")
     public Result<String> delete(@RequestParam String id) {
         return Result.success(elasticsearchRestTemplate.delete(id, Article.class));
     }
 
-    @GetMapping
-    @ApiOperation("搜索")
-    public Result<SearchHits<Article>> search(String keyWord) {
+    @ApiOperation("根据标题搜索")
+    @GetMapping("/search-title")
+    public Result<Page<Article>> searchByTitle(String title) {
 
         Pageable pageable = PageRequest.of(0,10);  // page 从第 0 页开始
+
+        return Result.success(articleRepository.findByTitleLike(title, pageable));
+    }
+
+    @GetMapping("/search-temp")
+    @ApiOperation("通过Template搜索")
+    public Result<SearchHits<Article>> search(String keyWord) {
+
+        // page 从第 0 页开始
+        Pageable pageable = PageRequest.of(0,10);
 
         HighlightBuilder.Field highlightField = new HighlightBuilder.Field("title")
                 .preTags("<span>")
